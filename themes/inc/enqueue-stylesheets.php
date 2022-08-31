ed47543dd580a735b7cdb42943cc8e3c27aa9973
Bertin <dim.bertin@gmail.com>
2022-08-31 15:17:00 +0200

    Add banner

--- themes/inc/enqueue-stylesheets.php
+++ themes/inc/enqueue-stylesheets.php
@@ -6,8 +6,8 @@
 
 function theme_register_styles()
 {
-  wp_enqueue_style('font-style', get_template_directory_uri() . '/assets/fonts.css', array(), rand(111, 9999), 'all');
-  wp_enqueue_style('theme-style', get_template_directory_uri() . '/public/assets/main.css', array(), rand(111, 9999), 'all');
+  // wp_enqueue_style('font-style', get_template_directory_uri() . '/assets/fonts.css', array(), rand(111, 9999), 'all');
+  // wp_enqueue_style('theme-style', get_template_directory_uri() . '/public/assets/main.css', array(), rand(111, 9999), 'all');
 }
 
 add_action('wp_enqueue_scripts', 'theme_register_styles');
